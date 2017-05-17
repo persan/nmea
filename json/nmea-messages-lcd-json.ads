5b287bd3993c67b102730161cc8f564b651fa264
Per Sandberg <per.sandberg@saabgroup.com>
2017-05-17 12:37:05 +0200

    Put file in correct place

--- json/nmea-messages-lcd-json.ads
+++ json/nmea-messages-lcd-json.ads
@@ -3,26 +3,10 @@ with GNATCOLL.JSON;
 package NMEA.Messages.LCD.JSON is
    use GNATCOLL.JSON;
 
-   ----------------------------------------------------
-   --  LCD_Message
-   ---------------------------------------------------
-   
    function Create (Val : LCD_Message) return JSON_Value;
-   
-   procedure Populate (Val : LCD_Message; Target : in out JSON_Value);
-
    function Get (Val : GNATCOLL.JSON.JSON_Value) return LCD_Message;
-   
-   function Get (Val   : JSON_Value;
-                 Field : UTF8_String) return LCD_Message;
-   
-   procedure Set_Field  (Val        : JSON_Value;
-                         Field_Name : UTF8_String;
-                         Field      : LCD_Message);
-   
-   procedure Map (Val : JSON_Value;
-                  To  : in out LCD_Message);   
-
-   procedure Map_Field (Name : UTF8_String; Value : JSON_Value; To : in out LCD_Message);
+   function Get (Val : JSON_Value; Field : UTF8_String) return LCD_Message;
+   procedure Set_Field  (Val        : JSON_Value;  Field_Name : UTF8_String; Field  : LCD_Message);
+   procedure Map(Val : JSON_Value; To : in out LCD_Message);   
 
 end NMEA.Messages.LCD.JSON;
